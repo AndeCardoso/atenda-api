8b67897f09db8c0e19c1fecebdf5ea1e2460b4e1
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-04-12 07:55:52 -0300

    feat: Add user update route and token validation in user create route

---
+++ src/modules/user/useCases/updateUser/UpdateUserController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { UpdateUserUseCase } from "./UpdateUserUseCase";
+import { validationResult } from "express-validator";
+import { ParamsError } from "@errors/ParamError";
+
+export class UpdateUserController {
+  async handle(req: Request, res: Response) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(new ParamsError(errors));
+    }
+    const userPayload = req.headers.user as string;
+    const { id: userId } = JSON.parse(userPayload!!);
+
+    const { id } = req.params;
+    const body = req.body;
+
+    const updateUserUseCase = new UpdateUserUseCase();
+
+    try {
+      const result = await updateUserUseCase.execute(
+        Number(id),
+        Number(userId),
+        body
+      );
+      return res.status(result.statusCode).json(result.body);
+    } catch (error) {
+      console.log(JSON.stringify(error, null, 2));
+    }
+  }
+}
