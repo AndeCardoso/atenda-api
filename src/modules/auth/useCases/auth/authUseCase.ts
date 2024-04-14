2d23547be95298c0bc82c577bd32718c02724a04
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-04-14 17:56:30 -0300

    feat: Add treatment for trial access

--- src/modules/auth/useCases/auth/authUseCase.ts
+++ src/modules/auth/useCases/auth/authUseCase.ts
@@ -6,6 +6,8 @@ import { AuthRequestDTO } from "@modules/auth/dtos/auth/AuthRequestDTO";
 import { AuthResponseDTO } from "@modules/auth/dtos/auth/AuthResponseDTO";
 import { badRequest, ok } from "@helper/http/httpHelper";
 import { HttpResponse } from "@shared/protocols/http";
+import { companyStatusEnum } from "@modules/company/constants";
+import { differenceInDays } from "date-fns";
 
 const secretKey = process.env.SECRET_KEY_JWT as jwt.Secret;
 const accessExpireTime = process.env.ACCESS_EXPIRE_TIME as jwt.Secret;
@@ -29,6 +31,34 @@ export class AuthUseCase {
 
     if (!match) {
       return badRequest("E-mail ou senha inválida");
+    }
+
+    const company = await prisma.company.findFirst({
+      where: {
+        id: user.companyId,
+      },
+    });
+
+    if (company?.status === companyStatusEnum.BLOCKED) {
+      return badRequest(
+        "Usuário bloqueado, entre em contato com o Sistema Atenda para reativar sua conta"
+      );
+    }
+
+    if (company?.status === companyStatusEnum.TRIAL) {
+      if (differenceInDays(new Date(), new Date(company.created_at)) > 15) {
+        await prisma.company.update({
+          where: {
+            id: company.id,
+          },
+          data: {
+            status: companyStatusEnum.BLOCKED,
+          },
+        });
+        return badRequest(
+          "Licensa gratuita encerrada, entre em contato com o Sistema Atenda para reativar sua conta"
+        );
+      }
     }
 
     const userPayload = {
