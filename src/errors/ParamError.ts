675797b1923f9f35c61c650e0bec9b1baf713a04
AndeCardoso <andersoncardoso.dev@gmail.com>
2024-04-20 08:25:22 -0300

    fix: Add own types for the stack error and validation error result

--- src/errors/ParamError.ts
+++ src/errors/ParamError.ts
@@ -1,20 +1,23 @@
-import {
-  FieldValidationError,
-  Result,
-  ValidationError,
-} from "express-validator";
+import { Result } from "express-validator";
+
+type TValidationError = {
+  path: string;
+  msg: string;
+};
+
+type TStackError = {
+  param: string;
+  message: string;
+};
 
 export class ParamsError {
   public readonly name: string;
-  public readonly stack: {
-    param: string;
-    message: string;
-  }[];
+  public readonly stack: TStackError[];
 
-  constructor(stack: Result<FieldValidationError> | Result<ValidationError>) {
+  constructor(stack: Result<TValidationError>) {
     this.name = "ParamsError";
     this.stack = stack.array().map((error) => {
-      return { param: error.type, message: error.msg };
+      return { param: error.path, message: error.msg };
     });
   }
 }
